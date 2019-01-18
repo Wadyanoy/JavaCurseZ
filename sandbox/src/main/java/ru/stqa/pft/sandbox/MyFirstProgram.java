0550230e42404994a3dc0c004947d8e3909666e0
Wadyanoy <Wadyanoy@gmail.com>
2019-01-18 17:17:42 +0300

    last

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,21 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        hello("world");
+        hello("Wadya");
+        Square s = new Square(5);
+        Rectangle r = new Rectangle(4,6);
+        System.out.println("Ploshad qadrata so storonoi " +s.len+" = " +area(s));
+
+        System.out.println("Ploshad pryamougolnika so storonami "+r.a+" i "+r.b+" = " +area(r));
+    }
+    public static  void hello(String somebody){
+        System.out.println("Hello, world!");
+    }
+    public static double area(Square s){
+        return s.len*s.len;
+    }
+    public static double area(Rectangle r){
+        return r.a*r.b;
     }
 }
