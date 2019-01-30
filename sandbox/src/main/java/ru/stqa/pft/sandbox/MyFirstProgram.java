670148dafe6ce23a289c9326a388ac833ee2ab93
Wadyanoy <Wadyanoy@gmail.com>
2019-01-30 15:40:12 +0300

    testWithSeleniumBuilder

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,18 +5,29 @@ public class MyFirstProgram {
         hello("world");
         hello("Wadya");
         Square s = new Square(5);
-        Rectangle r = new Rectangle(4,6);
-        System.out.println("Ploshad qadrata so storonoi " +s.len+" = " +area(s));
+        Rectangle r = new Rectangle(4, 6);
 
-        System.out.println("Ploshad pryamougolnika so storonami "+r.a+" i "+r.b+" = " +area(r));
+        Point p1= new Point (3,2);
+        Point p2 = new Point (5,6);
+
+        System.out.println("Ploshad qadrata so storonoi " + s.len + " = " + s.area());
+
+        System.out.println("Ploshad pryamougolnika so storonami " + r.a + " i " + r.b + " = " + r.area());
+        System.out.println(Math.sqrt((5-3)*(5-3)+(6-2)*(6-2)));
+        System.out.println("Otvet = "+ distance(p1,p2));
+
     }
-    public static  void hello(String somebody){
+
+    public static void hello(String somebody) {
         System.out.println("Hello, world!");
     }
-    public static double area(Square s){
-        return s.len*s.len;
+
+
+ public static double distance (Point p1, Point p2){
+
+        return  Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
     }
-    public static double area(Rectangle r){
-        return r.a*r.b;
-    }
+
 }
+
+
