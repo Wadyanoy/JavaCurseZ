6218b4411cf5ed526299d341e43c0e856d1bb411
Wadyanoy <Wadyanoy@gmail.com>
2019-02-22 17:04:51 +0300

    22 02 19

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,22 +1,28 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
 
 public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() {
         app.goTo().groupPage();
-        List<GroupData> before = app.group().list();
+        Groups before= app.group().all();
         GroupData group = new GroupData().withName("test2");
         app.group().create(group);
-        List<GroupData> after = app.group().list();
-        Assert.assertEquals(after.size(), before.size() + 1);
+        Groups after = app.group().all();
+        //Assert.assertEquals(after.size(), before.size() + 1);
+        MatcherAssert.assertThat(after.size(), CoreMatchers.equalTo(before.size() + 1));
 
        /* int max=0;
         for (GroupData g:after){
@@ -29,11 +35,8 @@ public class GroupCreationTests extends TestBase {
 
         //улучшенный вариант поиска максимального ид через лямбду
        // group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
-        before.add(group);
-        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(),g2.getId());
-        before.sort(byId);
-        after.sort(byId);
-        Assert.assertEquals(before,after);
+      //  Assert.assertEquals(before,after);
+        MatcherAssert.assertThat(after, CoreMatchers.equalTo(before.withAdded(group.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt()))));
     }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,19 +1,23 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
 
 public class GroupModificationTests extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions() {
         app.goTo().groupPage();
-        if (app.group().list().size() ==0) {
+        if (app.group().all().size() ==0) {
             app.group().create(new GroupData().withName("test1"));
         }
     }
@@ -21,20 +25,18 @@ public class GroupModificationTests extends TestBase {
     @Test
     public void testGroupModification() {
 
-        List<GroupData> before = app.group().list();
-        int index = before.size() - 1;
-        GroupData group = new GroupData().withId(before.get(index).getId()).withName( "edit2664").withHeader("edit3").withFooter("edit4");
-        app.group().modify(index, group);
-        List<GroupData> after = app.group().list();
-        Assert.assertEquals(after.size(), before.size());
+        Groups before = app.group().all();
+        GroupData modifiedGroup = before.iterator().next();
 
-        before.remove(index);
-        before.add(group);
-        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
-        before.sort(byId);
-        after.sort(byId);
+        GroupData group = new GroupData().withId(modifiedGroup.getId()).withName( "edit2664").withHeader("edit3").withFooter("edit4");
+        app.group().modify(group);
+        Groups after = app.group().all();
+        Assert.assertEquals(after.size(), before.size()); //можно сократить запись , как в других классах спомощью импорта статических методов
 
-        Assert.assertEquals(before, after);
+        MatcherAssert.assertThat(after,     //можно сократить запись , как в других классах спомощью импорта статических методов
+                CoreMatchers.equalTo(before
+                        .without(modifiedGroup)
+                        .withAdded(group)));
 
     }
 
